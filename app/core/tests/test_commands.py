4ae0a22835bb5987d637dba4b35d79fdee4099eb
mkodz <m.kodz@wp.pl>
2023-10-12 21:45:57 +0200

    fixed Unit Tests

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,7 +2,9 @@
 Test custom Django management commands.
 """
 from unittest.mock import patch
+
 from psycopg2 import OperationalError as Psycopg2OpError
+
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
@@ -11,13 +13,13 @@ from django.test import SimpleTestCase
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTest(SimpleTestCase):
 
-    def test_wait_for_db_ready(self, patch_checked):
+    def test_wait_for_db_ready(self, patched_check):
         """test waiting for db if db ready."""
-        patch_checked.return_value = True
+        patched_check.return_value = True
 
         call_command('wait_for_db')
 
-        patch_checked.assert_called_once_with(databases=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
